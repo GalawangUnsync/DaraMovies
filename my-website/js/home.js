023f4db60272ef9aa43d41d15594c1a1137d51a6
GalawangUnsync <jayvee.orain@gmail.com>
2025-05-21 22:15:58 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b49372f7598e34c677a95ce417837a12a';
+const API_KEY = '49372f7598e34c677a95ce417837a12a';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
